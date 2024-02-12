83e94a394983ed7b796651a67a947371722fdcf7
Lucas-TA <dev.ascencao@gmail.com>
2024-02-12 18:09:33 -0300

    ongoing

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,53 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+    function calculateResult(): number | string {
+        const firstNumber: number = parseFloat(this.firstNumberInput.toString());
+        const secondNumber: number = parseFloat(this.secondNumberInput.toString());
+
+        switch (this.selectedOperation) {
+            case 'sum':
+                return firstNumber + secondNumber;
+            case 'minus':
+                return firstNumber - secondNumber;
+            case 'multiply':
+                return firstNumber * secondNumber;
+            case 'divide':
+                if (secondNumber !== 0) {
+                    return firstNumber / secondNumber;
+                } else {
+                    return 'Indefinido';
+                }
+            default:
+                return 'Operação Inválida';
+        }
+    }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <header class="container">
+        <h1 class="text-center p-5"> Calculadora Aritmética</h1>
+    </header>
+    <main class="container d-flex flex-column justify-content-center align-items-center">
+        <h3 class="text-center">Digite dois números e selecione a operação desejada:</h3>
+        <div class="text-center d-flex justify-content-center align-items-center p-5">
+            <input @change="(e:any) => {if (e.target) SetNumbers.firstNumber = (e.target as HTMLInputElement).value}" type="number" placeholder="Digite um número">
+            <select @change="(e:any)=> { if (e.target) state.filter = (e.target as HTMLOptionElement).value }" class="btn btn-primary text-center d-flex me-2 ms-2">
+                <option value="sum">+</option>
+                <option value="minus">-</option>
+                <option value="multiply">x</option>
+                <option value="divide">&#247;</option>
+            </select>
+            <input  @change="(e:any) => {if (e.target) secondNumberInput = (e.target as HTMLInputElement).value}" type="number" placeholder="Digite um número">
+        </div>
+        <span>Resultado: {{ calculateResult() }}</span>
+    </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+    input {
+        height: 30px;
+    }
+    select {
+        font-size: 18px;
+    }
 </style>
