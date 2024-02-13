34a821c43065dbc7c4cf592a9d6bb07f6bf817ac
Lucas Ascenção <dev.ascencao@gmail.com>
2024-02-13 12:52:18 -0300

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <script setup lang="ts">
+    import { ref } from 'vue';
+    
+    const firstNumber = ref<number | string>(0);
+    const secondNumber = ref<number | string>(0);
+    const signal = ref<string>('sum');
 
-    function calculateResult(): number | string {
-        const firstNumber: number = parseFloat(this.firstNumberInput.toString());
-        const secondNumber: number = parseFloat(this.secondNumberInput.toString());
+    function calculate(): number | string {
+    const num1 = parseFloat(firstNumber.value.toString());
+    const num2 = parseFloat(secondNumber.value.toString());
+    
+    if (isNaN(num1) || isNaN(num2)) {
+        return "Digite os valores nos campos disponíveis";
+    }
 
-        switch (this.selectedOperation) {
+        switch (signal.value) {
             case 'sum':
-                return firstNumber + secondNumber;
+                return num1 + num2;
             case 'minus':
-                return firstNumber - secondNumber;
+                return num1 - num2;
             case 'multiply':
-                return firstNumber * secondNumber;
+                return num1 * num2;
             case 'divide':
-                if (secondNumber !== 0) {
-                    return firstNumber / secondNumber;
-                } else {
-                    return 'Indefinido';
+                if (num2 === 0) {
+                    return "Cannot divide by zero";
                 }
+                return num1 / num2;
             default:
-                return 'Operação Inválida';
+                return "Operação inválida";
         }
     }
+
 </script>
 
 <template>
@@ -30,16 +39,24 @@
     <main class="container d-flex flex-column justify-content-center align-items-center">
         <h3 class="text-center">Digite dois números e selecione a operação desejada:</h3>
         <div class="text-center d-flex justify-content-center align-items-center p-5">
-            <input @change="(e:any) => {if (e.target) SetNumbers.firstNumber = (e.target as HTMLInputElement).value}" type="number" placeholder="Digite um número">
-            <select @change="(e:any)=> { if (e.target) state.filter = (e.target as HTMLOptionElement).value }" class="btn btn-primary text-center d-flex me-2 ms-2">
+            <input
+                @input="firstNumber = ($event.target as HTMLInputElement).value"
+                type="number" 
+                placeholder="Digite um número"
+            >
+            <select v-model="signal" class="btn btn-primary text-center d-flex me-2 ms-2">
                 <option value="sum">+</option>
                 <option value="minus">-</option>
                 <option value="multiply">x</option>
                 <option value="divide">&#247;</option>
             </select>
-            <input  @change="(e:any) => {if (e.target) secondNumberInput = (e.target as HTMLInputElement).value}" type="number" placeholder="Digite um número">
+            <input
+                @input="secondNumber = ($event.target as HTMLInputElement).value"
+                type="number" 
+                placeholder="Digite um número"
+            >
         </div>
-        <span>Resultado: {{ calculateResult() }}</span>
+        <span>Resultado: {{ calculate() }}</span>
     </main>
 </template>
 
